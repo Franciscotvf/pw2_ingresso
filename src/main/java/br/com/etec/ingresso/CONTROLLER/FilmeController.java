d2a6b8c1a9abe4ecb8b0b6b933bc05cf5e963f93
Francisco&Gustavo <frantvfilho@hotmail.com>
2026-08-17 08:52:37 -0300

    Criação RESPONSE de erros, post e delete

--- src/main/java/br/com/etec/ingresso/CONTROLLER/FilmeController.java
+++ src/main/java/br/com/etec/ingresso/CONTROLLER/FilmeController.java
@@ -4,6 +4,8 @@ import br.com.etec.ingresso.ENTITY.Filme;
 import br.com.etec.ingresso.ENUMS.ClassificacaoIndicativaENUM;
 import br.com.etec.ingresso.ENUMS.SimNaoEnum;
 import lombok.Builder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,45 +16,63 @@ import java.util.List;
 
 public class FilmeController {
 
+    List<Long> idExistentes = List.of(1L, 2L, 3L);
     @GetMapping
-    public List<Filme> listar() {
-        Filme filme1 = Filme.builder()
-                .id(1L) // Dado long
-                .titulo("Matrix")
-                .classificacao(ClassificacaoIndicativaENUM.A16)
-                .cartaz(SimNaoEnum.S)
-                .build();
+    public ResponseEntity<Filme> buscarPorId(@PathVariable Long id) {
+        if (idExistentes.contains(id)) {
+            Filme filme1 = Filme.builder().id(1L)
+                    .id(1L) // Dado long
+                    .titulo("Matrix")
+                    .classificacao(ClassificacaoIndicativaENUM.A16)
+                    .cartaz(SimNaoEnum.S)
+                    .build();
 
-        Filme filme2 = Filme.builder()
-                .id(2L)
-                .titulo("Avatar")
-                .classificacao(ClassificacaoIndicativaENUM.A18)
-                .cartaz(SimNaoEnum.S)
-                .build();
+            Filme filme2 = Filme.builder().id(2L)
+                    .id(2L)
+                    .titulo("Avatar")
+                    .classificacao(ClassificacaoIndicativaENUM.A18)
+                    .cartaz(SimNaoEnum.S)
+                    .build();
 
-        Filme filme3 = Filme.builder().id(3L)
-                .titulo("Homem aranha: Um novo dia")
-                .classificacao(ClassificacaoIndicativaENUM.A16)
-                .cartaz(SimNaoEnum.N)
-                .build();
+            Filme filme3 = Filme.builder().id(3L)
+                    .titulo("Homem aranha: Um novo dia")
+                    .classificacao(ClassificacaoIndicativaENUM.A16)
+                    .cartaz(SimNaoEnum.N)
+                    .build();
 
-        return List.of(filme1, filme2, filme3);
+            return ResponseEntity.ok(filme1);
+        }
+        return ResponseEntity.notFound().build();
     }
     @GetMapping("/{id}") //Buscar variavel de filmes através de /{id}
     public Filme BuscarPorto(@PathVariable Long id){ //PathVariable para puxar os id
-        Filme filme2 = Filme.builder()
+        Filme filme1 = Filme.builder()
                 .id(id)
-                .titulo("Avatar")
+                .titulo("Matrix")
                 .classificacao(ClassificacaoIndicativaENUM.A18)
                 .cartaz(SimNaoEnum.S)
                 .build();
-        return filme2;
+        return filme1;
 
 
     }
     @PostMapping
-    public Filme cadastrar(@RequestBody Filme filme){
+    public ResponseEntity<Filme> cadastrar(@RequestBody Filme filme){
         filme.setId(100L);
-        return filme;
+        return ResponseEntity.status(HttpStatus.CREATED).body(filme);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Filme> atualizar(@RequestBody Filme filme, @PathVariable Long id){
+        if(idExistentes.contains(id)){
+            return ResponseEntity.ok(filme);
+        }
+    return ResponseEntity.notFound().build();
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar (@PathVariable Long id){
+        if(idExistentes.contains(id)) {
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
